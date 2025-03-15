0930daa4ebae421a20f251f80d98f0de07d252b5
psyha9 <psyha9@exmail.nottingham.ac.uk>
2025-03-15 16:19:28 +1300

    renaming starting screen

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -6,10 +6,10 @@ export default function Start() {
       className={`${font.className} flex flex-col justify-center items-center h-screen`}
     >
       <h1 className="text-[#a31010] text-8xl text-center my-5">
-        ENTER THE TARDIS
+        ENTER THE THUNERDOME
       </h1>
       <h3 className="text-[#ffbd59] text-2xl text-center my-5">
-        Terrible And Really Duff Information System
+        be reborn through the weather
       </h3>
       <a href={`/dates`}>
         <button className="bg-[#a31010] text-black text-2xl w-[250px] h-[50px] rounded-2xl my-5">
